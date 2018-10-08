6ae0f98a431b9999a3395251353b7f86c96dd3ef
Alkesst <alec.augal@gmail.com>
2018-10-08 18:58:51 +0200

    Added exercise 3.3 (Discover devices by address or device name)

--- Alec/Bluetooth/src/Main.java
+++ Alec/Bluetooth/src/Main.java
@@ -1,56 +1,25 @@
+import bluetooth.DeviceSeeker;
+
 import javax.bluetooth.*;
 import java.io.IOException;
 
 public class Main {
+
     private static final Object inquiryCompletedEvent = new Object();
+
     public static void main(String[] args) throws BluetoothStateException, InterruptedException {
+        /* Show local device bluetooth information (Exercise 2.1) */
         LocalDevice localDevice = LocalDevice.getLocalDevice();
         System.out.println("Local Address: " + localDevice.getBluetoothAddress());
         System.out.println("Device Class: " + localDevice.getDeviceClass());
         System.out.println("Friendly Name: " + localDevice.getFriendlyName());
         System.out.println("Discoverable Mode: " + localDevice.getDiscoverable());
-
+        /*
+        * Discover all near devices (Exercise 3.2)
+        * */
         DiscoveryAgent agent = localDevice.getDiscoveryAgent();
-        DiscoveryListener discoveryListener = new DiscoveryListener() {
-            @Override
-            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
-                try {
-                    System.out.printf("    – %s (%s)\n", btDevice.getFriendlyName(false), btDevice.getBluetoothAddress());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
-                for (ServiceRecord s : servRecord) {
-                    DataElement serviceName = s.getAttributeValue(0x0100);
-                    if (serviceName != null) {
-                        System.out.println((String) serviceName.getValue());
-                    } else {
-                        System.out.println("    – Service without name.");
-                    }
-                    System.out.println(s.getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false));
-                }
-            }
-
-            @Override
-            public void serviceSearchCompleted(int transID, int respCode) {
-                System.out.println("    " + respCode);
-                synchronized (inquiryCompletedEvent) {
-                    inquiryCompletedEvent.notifyAll();
-                }
-            }
-
-            @Override
-            public void inquiryCompleted(int discType) {
-                System.out.println("============= Process Done... =============");
-                synchronized (inquiryCompletedEvent) {
-                    inquiryCompletedEvent.notifyAll();
-                }
-            }
-        };
-        System.out.println("============= Searching devices... =============");
+        DiscoveryListener discoveryListener = new DeviceSeeker(inquiryCompletedEvent);
+        System.out.println("============= \tSearching devices\t =============");
         agent.startInquiry(DiscoveryAgent.GIAC, discoveryListener);
         synchronized (inquiryCompletedEvent) {
             try {
@@ -59,18 +28,25 @@ public class Main {
                 e.printStackTrace();
             }
         }
-        for (RemoteDevice device : agent.retrieveDevices(DiscoveryAgent.CACHED)) {
-            agent.searchServices(new int[]{0x0100}, new UUID[]{new UUID(0x1002)}, device, discoveryListener);
-            try {
-                System.out.println("  > " + device.getFriendlyName(false));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            synchronized (inquiryCompletedEvent) {
-                inquiryCompletedEvent.wait();
-            }
-            System.out.println();
+        /* Discover a device using Name or Bluetooth address (exercise 3.3) */
 
+        /* SERVICE DISCOVERY FOR ALL NEAR DEVICES (exercise 4.5) */
+        RemoteDevice[] cachedDevices = agent.retrieveDevices(DiscoveryAgent.CACHED);
+        if (cachedDevices != null) {
+            for (RemoteDevice device : cachedDevices) {
+                agent.searchServices(new int[]{0x0100}, new UUID[]{new UUID(0x1002)}, device, discoveryListener);
+                try {
+                    System.out.println("\t> " + device.getFriendlyName(false));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                synchronized (inquiryCompletedEvent) {
+                    inquiryCompletedEvent.wait();
+                }
+                System.out.println();
+            }
+        } else {
+            System.err.println("No devices found");
         }
     }
 }
