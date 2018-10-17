9247b9adf7856ce34b6896f58f12cb10c9aa2fd6
Alkesst <alec.augal@gmail.com>
2018-10-17 20:55:16 +0200

    Changes

--- Aleks/Bluetooth/src/bluetooth/BluetoothSocket.java
+++ Aleks/Bluetooth/src/bluetooth/BluetoothSocket.java
@@ -4,40 +4,58 @@ import javax.bluetooth.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class BluetoothSocket {
     private static final Object inquiryCompletedEvent = new Object();
     public static void main(String[] args) throws IOException, InterruptedException {
         LocalDevice localDevice = LocalDevice.getLocalDevice();
         DiscoveryAgent agent = localDevice.getDiscoveryAgent();
-        DiscoveryListener discoveryListener;
-        String deviceAddress;
-        RemoteDevice[] cachedDevices;
+        DeviceAndServiceDiscoverer discoveryListener;
+        List<RemoteDevice> cachedDevices;
+        String serverBluetoothAddress;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.print("You know the name or bluetooth address from the server? (true if the answer is yes): ");
         boolean theUserKnowsTheBluetoothAddress = Boolean.valueOf(br.readLine());
         if (!theUserKnowsTheBluetoothAddress) {
-            System.out.println("You know the name of the device to connect to? ");
+            System.out.print("You know the name of the device to connect to? ");
             boolean theUserKnowsTheDeviceName = Boolean.valueOf(br.readLine());
             if (theUserKnowsTheDeviceName) {
+                System.out.print("What is the device name? ");
                 discoveryListener = new DeviceAndServiceDiscoverer(inquiryCompletedEvent,
                         br.readLine(), false);
+                System.out.println("============= \tSearching near devices\t =============");
                 agent.startInquiry(DiscoveryAgent.GIAC, discoveryListener);
                 synchronized (inquiryCompletedEvent) {
                     inquiryCompletedEvent.wait();
                 }
-                cachedDevices = agent.retrieveDevices(DiscoveryAgent.CACHED);
-                // check if the device is contained in the data structure, then, take the bluetooth address.
+                cachedDevices = discoveryListener.getCachedDevices();
+                if (cachedDevices.isEmpty()) {
+                    System.err.println("Device not found...");
+                    System.exit(-1);
+                }
+                serverBluetoothAddress = cachedDevices.get(0).getBluetoothAddress();
             } else {
-                // Search near devices, add them into a dictionary, then, ask the user what is the
-                // device to connect to and just get the value from the key. (is better a dictionary or an array???)
+                System.out.println("============= \tSearching near devices\t =============");
+                discoveryListener = new DeviceAndServiceDiscoverer(inquiryCompletedEvent);
+                agent.startInquiry(DiscoveryAgent.GIAC, discoveryListener);
+                synchronized (inquiryCompletedEvent) {
+                    inquiryCompletedEvent.wait();
+                }
+                cachedDevices = discoveryListener.getCachedDevices();
+                if (cachedDevices.isEmpty()) {
+                    System.err.println("No devices found...");
+                    System.exit(-1);
+                }
+                discoveryListener.printAllDevices();
+                System.out.print("Tell me the number of the device you are going to connect: ");
+                int indexOfDevice = Integer.valueOf(br.readLine()) - 1;
+                serverBluetoothAddress = cachedDevices.get(indexOfDevice).getBluetoothAddress();
             }
         } else {
             System.out.print("Tell me the bluetooth address: ");
-            deviceAddress = br.readLine();
+            serverBluetoothAddress = br.readLine();
         }
-
-
     }
 }
